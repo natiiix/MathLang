ec212a342794879946a2872412ce6027dbd671b7
Ivo Meixner <ivo.meixner@se7en.cz>
2017-09-26 01:29:51 +0200

    Created the Fraction class.

--- MathLang/Program.cs
+++ MathLang/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace MathLang
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
         }
     }
